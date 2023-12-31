3686a27bafa3f272f1977111a341762ccde55689
Moyudin Papu <moinpapu92446@gmail.com>
2023-12-31 21:51:21 +0530

    I have completed morgan website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch , Route } from "react-router-dom/cjs/react-router-dom";
+import Header from './Components/Header';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import ContactUs from './pages/ContactUs';
+import Footer from './Components/Footer';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/"  component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Service' component={Service}/>
+        <Route path='/ContactUs' component={ContactUs}/>
+      </Switch>
+      <Footer/>
+    </Router>
+    
+    </>
   );
 }
 
